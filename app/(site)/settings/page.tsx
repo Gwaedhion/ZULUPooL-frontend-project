cc4a37319b1a3fab1d373db4bb355cc1692af860
Gwaedhion <molodkin95@yandex.ru>
2024-04-19 16:53:17 +0300

    account settings

--- app/(site)/settings/page.tsx
+++ app/(site)/settings/page.tsx
@@ -2,7 +2,6 @@
 import { useEffect, useRef, useState } from 'react';
 import styles from './page.module.css';
 import cn from 'classnames';
-import Link from 'next/link';
 import PaymentsSettingsIcon from '../../../public/settings-icons/payments-settings-icon.svg';
 import AccountSettingsIcon from '../../../public/settings-icons/account-settings-icon.svg';
 import CoinButtons from '@/components/CoinButtons/CoinButtons';
@@ -53,9 +52,6 @@ export default function Info() {
 		console.log(apiData);
 	}, []);
 
-	const [coinToShow, setCoinToShow] =
-		useState<IUserGetSettingsCoins | null>();
-
 	const [userLanguage, setUserLanguage] = useState(
 		localStorage.getItem('userLang')
 	);
@@ -74,55 +70,109 @@ export default function Info() {
 		}
 	}, [userLanguage]);
 
+	const arr = apiData?.coins;
+
+	console.log(arr);
+
+	const [paymentAddressPlaceholder, setPaymentAddressPlaceholder] =
+		useState('');
+
+	const getPaymentAddressPlaceholder = () => {
+		arr?.forEach((element) => {
+			if (element.name == currentSecondaryCoin.title) {
+				setPaymentAddressPlaceholder(element.address);
+			}
+		});
+		console.log(paymentAddressPlaceholder);
+	};
+
+	useEffect(() => {
+		getPaymentAddressPlaceholder();
+	});
+
+	useEffect(() => {
+		getPaymentAddressPlaceholder();
+	}, [currentSecondaryCoin]);
+
+	const [payoutThresholdPlaceholder, setPayoutThresholdPlaceholder] =
+		useState('');
+
+	const getPayoutThresholdPlaceholder = () => {
+		arr?.forEach((element) => {
+			if (element.name == currentSecondaryCoin.title) {
+				setPayoutThresholdPlaceholder(element.payoutThreshold);
+			}
+		});
+		console.log(payoutThresholdPlaceholder);
+	};
+
+	useEffect(() => {
+		getPayoutThresholdPlaceholder();
+	});
+
+	useEffect(() => {
+		getPayoutThresholdPlaceholder();
+	}, [currentSecondaryCoin]);
+
+	const coin = userLang.current.settings['payment-settings-form'].coins;
+
 	return (
 		<div className={styles.pageWrapper}>
 			<div className={styles.settingsContainer}>
 				<div className={styles.settingsPaymentOption}>
-					<h3
-						className={cn(styles.settingsTitle, {
-							[styles.settingsTitle_active]:
+					<button
+						className={cn(styles.settingsButton, {
+							[styles.settingsButton_activeBlue]:
 								settingsState == 'payment-settings',
 						})}
-					>
-						{
-							userLang.current.settings['payment-settings-form']
-								.settings.paymentSettings.title
-						}
-					</h3>
-					<button
-						className={styles.settingsButton}
 						onClick={() => setSettingsState('payment-settings')}
 					>
+						<p
+							className={cn(styles.settingsTitle, {
+								[styles.settingsTitle_activeBlue]:
+									settingsState == 'payment-settings',
+							})}
+						>
+							{
+								userLang.current.settings[
+									'payment-settings-form'
+								].settings.paymentSettings.title
+							}
+						</p>
 						<PaymentsSettingsIcon
 							className={cn(styles.settingsIcon, {
-								[styles.settingsIcon_active]:
+								[styles.settingsIcon_activeBlue]:
 									settingsState == 'payment-settings',
 							})}
 						/>
 					</button>
 				</div>
 				<div className={styles.settingsAccountOption}>
-					<h3
-						className={cn(styles.settingsTitle, {
-							[styles.settingsTitle_active]:
+					<button
+						className={cn(styles.settingsButton, {
+							[styles.settingsButton_activePurple]:
 								settingsState == 'account-settings',
 						})}
-					>
-						{
-							userLang.current.settings['payment-settings-form']
-								.settings.accountSettings.title
-						}
-					</h3>
-					<button
-						className={styles.settingsButton}
 						onClick={() => setSettingsState('account-settings')}
 					>
 						<AccountSettingsIcon
 							className={cn(styles.settingsIcon, {
-								[styles.settingsIcon_active]:
+								[styles.settingsIcon_activePurple]:
 									settingsState == 'account-settings',
 							})}
 						/>
+						<p
+							className={cn(styles.settingsTitle, {
+								[styles.settingsTitle_activePurple]:
+									settingsState == 'account-settings',
+							})}
+						>
+							{
+								userLang.current.settings[
+									'payment-settings-form'
+								].settings.accountSettings.title
+							}
+						</p>
 					</button>
 				</div>
 			</div>
@@ -147,7 +197,40 @@ export default function Info() {
 							type="text"
 							name="address"
 							id="address"
-							// placeholder={}
+							value={
+								paymentAddressPlaceholder !== null
+									? paymentAddressPlaceholder
+									: ''
+							}
+							placeholder={
+								currentSecondaryCoin.title == 'BTC'
+									? coin.sha256.BTC.P2PKH
+									: currentSecondaryCoin.title == 'BCHN'
+									? coin.sha256.BCHN.P2PKH
+									: currentSecondaryCoin.title == 'BSV'
+									? coin.sha256.BSV.P2PKH
+									: currentSecondaryCoin.title == 'DGB.sha256'
+									? coin.sha256['DGB.sha256'].P2PKH
+									: currentSecondaryCoin.title == 'HTR'
+									? coin.sha256.HTR.P2PKH
+									: currentSecondaryCoin.title == 'XEC'
+									? coin.sha256.XEC.P2PKH
+									: currentSecondaryCoin.title == 'DGB.odo'
+									? coin.odocrypt['DGB.odo'].P2PKH
+									: currentSecondaryCoin.title == 'DGB.qubit'
+									? coin.qubit['DGB.qubit'].P2PKH
+									: currentSecondaryCoin.title == 'DOGE'
+									? coin.scrypt.DOGE.P2PKH
+									: currentSecondaryCoin.title == 'DGB.scrypt'
+									? coin.scrypt['DGB.scrypt'].P2PKH
+									: currentSecondaryCoin.title == 'LTC'
+									? coin.scrypt.LTC.P2PKH
+									: currentSecondaryCoin.title == 'DGB.skein'
+									? coin.skein['DGB.skein'].P2PKH
+									: currentSecondaryCoin.title == 'XPM'
+									? coin.PrimePOW.XPM.P2PKH
+									: ''
+							}
 						/>
 						<label className={styles.formLabel} htmlFor="threshold">
 							{
@@ -162,6 +245,40 @@ export default function Info() {
 							type="text"
 							name="threshold"
 							id="threshold"
+							value={
+								payoutThresholdPlaceholder !== null
+									? `minPay= ${payoutThresholdPlaceholder} ${currentSecondaryCoin.title}`
+									: ''
+							}
+							placeholder={
+								currentSecondaryCoin.title == 'BTC'
+									? 'minPay= 0.001 BTC'
+									: currentSecondaryCoin.title == 'BCHN'
+									? 'minPay= 0.005 BCHN'
+									: currentSecondaryCoin.title == 'BSV'
+									? 'minPay= 0.005 BSV'
+									: currentSecondaryCoin.title == 'DGB.sha256'
+									? 'minPay= 10 DGB.sha256'
+									: currentSecondaryCoin.title == 'HTR'
+									? 'minPay= 8 HTR'
+									: currentSecondaryCoin.title == 'XEC'
+									? 'minPay= 10000 XEC'
+									: currentSecondaryCoin.title == 'DGB.odo'
+									? 'minPay= 10 DGB.odo'
+									: currentSecondaryCoin.title == 'DGB.qubit'
+									? 'minPay= 10 DGB.qubit'
+									: currentSecondaryCoin.title == 'DOGE'
+									? 'minPay= 100 DOGE'
+									: currentSecondaryCoin.title == 'DGB.scrypt'
+									? 'minPay= 10 DGB.scrypt'
+									: currentSecondaryCoin.title == 'LTC'
+									? 'minPay= 0.01 LTC'
+									: currentSecondaryCoin.title == 'DGB.skein'
+									? 'minPay= 10 DGB.skein'
+									: currentSecondaryCoin.title == 'XPM'
+									? 'minPay= 10 XPM'
+									: ''
+							}
 						/>
 						<div className={styles.formSwitchContainer}>
 							<Switch
@@ -170,11 +287,13 @@ export default function Info() {
 									setSwitchCheck(checked);
 								}}
 							/>
-							{
-								userLang.current.settings[
-									'payment-settings-form'
-								].settings.paymentSettings.autoPayoutEnabled
-							}
+							<p className={styles.switchText}>
+								{
+									userLang.current.settings[
+										'payment-settings-form'
+									].settings.paymentSettings.autoPayoutEnabled
+								}
+							</p>
 						</div>
 						<Button
 							className={styles.formButton}
@@ -187,7 +306,16 @@ export default function Info() {
 							}
 						</Button>
 					</form>
+					<div className={styles.infoBox}>
+						<p className={styles.infoText}>
+							{`An example of supported address formats and minimal
+							allowed payout for ${currentSecondaryCoin.title}:`}
+						</p>
+					</div>
 				</div>
+			)}
+			{settingsState == 'account-settings' && (
+				<div className={styles.settingsBox}></div>
 			)}
 		</div>
 	);
